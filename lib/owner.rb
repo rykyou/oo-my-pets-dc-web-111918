b8ebb70f383f8a2665507ab23e28b613c7052f66
Robin Kim <rkim1214@gmail.com>
2018-11-28 15:22:21 +0000

    Done.

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,63 +1,63 @@
 class Owner
   @@all = []
-  
+
   attr_accessor :name, :pets
   attr_reader :species
-  
+
   def initialize(species)
     @species = species
     @pets = {:fishes => [], :dogs => [], :cats => []}
     @@all << self
-  end 
-  
-  def self.all 
+  end
+
+  def self.all
     @@all
-  end 
-  
+  end
+
   def self.count
     self.all.count
-  end 
-  
-  def self.reset_all 
-    self.all.clear
-  end 
-  
+  end
+
+  def self.reset_all
+    self.all.clear  #self here, b/c it's in body of class method, refers to class
+  end
+
   def say_species
-    "I am a #{species}."
-  end 
-  
+    "I am a #{self.species}."
+  end
+
   def buy_fish(fish_name)
-    @pets[:fishes] << Fish.new(fish_name)
-  end 
-  
+    self.pets[:fishes] << Fish.new(fish_name)
+  end
+
   def buy_cat(cat_name)
-    @pets[:cats] << Cat.new(cat_name)
-  end 
-  
+    self.pets[:cats] << Cat.new(cat_name)
+  end
+
   def buy_dog(dog_name)
-    @pets[:dogs] << Dog.new(dog_name)
-  end 
-  
-  def walk_dogs 
-    @pets[:dogs].each { |dog| dog.mood = "happy"}
-  end 
-  
+    self.pets[:dogs] << Dog.new(dog_name)
+  end
+
+  def walk_dogs
+    self.pets[:dogs].each { |dog| dog.mood = "happy"}
+  end
+
   def play_with_cats
-    @pets[:cats].each { |cat| cat.mood = "happy"}
-  end 
-  
-  def feed_fish 
-    @pets[:fishes].each { |fish| fish.mood = "happy"}
-  end 
-  
+    self.pets[:cats].each { |cat| cat.mood = "happy"}
+  end
+
+  def feed_fish
+    self.pets[:fishes].each { |fish| fish.mood = "happy"}
+  end
+
   def sell_pets
-    @pets.each do |species, animals| 
+    self.pets.each do |species, animals|
       animals.each { |animal| animal.mood = "nervous"}
       animals.clear
     end
-  end 
-  
+  end
+
   def list_pets
-    "I have #{@pets[:fishes].count} fish, #{@pets[:dogs].count} dog(s), and #{@pets[:cats].count} cat(s)."
-  end 
+    "I have #{self.pets[:fishes].count} fish, #{self.pets[:dogs].count} dog(s), and #{self.pets[:cats].count} cat(s)."
+  end
 end
